0ca132c8cb0d138958dc74d534cd8753a42b6a89
mmrydz <megrydzewski@gmail.com>
2019-05-12 18:34:34 -0400

    added more components and additional pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,22 @@
 import React from "react";
-import Books from "./pages/Books";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Home from "./pages/Home";
+import Saved from "./pages/Saved";
+import NoMatch from "./pages/NoMatch";
 import Nav from "./components/Nav";
 
 function App() {
   return (
-    <div>
-      <Nav />
-      <Books />
-    </div>
+    <Router>
+      <div>
+        <Nav />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/saved" component={Saved} />
+          <Route component={NoMatch} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
---
+++ client/src/components/Footer/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer>
+      <hr />
+      <center> <i className="fab fa-github" /> 2019 | Meg Rydzewski
+      </center>  
+    </footer>
+  );
+}
+
+export default Footer;
